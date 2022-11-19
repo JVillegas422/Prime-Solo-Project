560b81cda366421b7d7154cf5ac60b23435c6960
Juan Villegas <jvillegas422@gmail.com>
2022-11-19 13:04:32 -0600

    Setup search saga

---
+++ src/redux/sagas/search.saga.js
@@ -0,0 +1,24 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchSearch(action) {
+    console.log('in fetchSearch', action.payload);
+    try {
+        const response = yield axios.get(`/api/search/${action.payload}`);
+        let medications = response.data.results.map(products => (products.products));
+        let brandName = medications.map(items => (items.map(nameItem => (nameItem.brand_name))));
+        yield put({
+            type: 'SET_SEARCH',
+            payload: brandName
+        });
+    }
+    catch (error) {
+        console.error('Search request failed', error)
+    }
+}
+
+function* searchSaga() {
+    yield takeLatest('FETCH_SEARCH', fetchSearch);
+}
+
+export default searchSaga;

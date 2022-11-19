fc7086f1e9259eadf760891e3798e8079396db76
Juan Villegas <jvillegas422@gmail.com>
2022-11-19 12:48:10 -0600

    Setup base SearchForm page

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,14 @@
+import { useDispatch, useSelector } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+
+function SearchForm() {
+    console.log('in SearchForm');
+
+    return (
+        <>
+            <h2>Search Form</h2>
+        </>
+    );
+}
+
+export default SearchForm;

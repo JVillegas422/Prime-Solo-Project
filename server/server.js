d4159bdb71f256f0e802dcc34c0c8d60961f5ea6
Juan Villegas <jvillegas422@gmail.com>
2022-11-19 13:14:39 -0600

    Setup search router & add router to server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 
+// Search router
+const searchRouter = require('./routes/search.router');
+
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -23,6 +26,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/search', searchRouter);
 
 // Serve static files
 app.use(express.static('build'));

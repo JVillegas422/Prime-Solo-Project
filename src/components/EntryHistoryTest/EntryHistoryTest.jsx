289e60834a931927c9c58f07fe64cdb9cfdde81a
Juan Villegas <jvillegas422@gmail.com>
2022-11-26 01:34:21 -0600

    Update EntryHistory table & add edit-delete table cells

--- src/components/EntryHistoryTest/EntryHistoryTest.jsx
+++ src/components/EntryHistoryTest/EntryHistoryTest.jsx
@@ -4,7 +4,6 @@ import { useHistory, useParams } from 'react-router';
 
 // MUI
 import PropTypes from 'prop-types';
-import { alpha } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -14,16 +13,12 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
+import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
 import { visuallyHidden } from '@mui/utils';
 
 function descendingComparator(a, b, orderBy) {
@@ -87,10 +82,22 @@ const headCells = [
     disablePadding: false,
     label: 'Notes ',
   },
+  {
+    id: 'delete',
+    numeric: true,
+    disablePadding: false,
+    label: 'Delete',
+  },
+  {
+    id: 'edit',
+    numeric: true,
+    disablePadding: false,
+    label: 'Edit',
+  },
 ];
 
 function EnhancedTableHead(props) {
-  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+  const { order, orderBy, onRequestSort } =
     props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
@@ -99,16 +106,7 @@ function EnhancedTableHead(props) {
   return (
     <TableHead>
       <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all prescriptions',
-            }}
-          />
+        <TableCell >
         </TableCell>
         {headCells.map((headCell) => (
           <TableCell
@@ -137,112 +135,23 @@ function EnhancedTableHead(props) {
 }
 
 EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
 };
 
-function EnhancedTableToolbar(props) {
-  const { numSelected } = props;
-  const rows = useSelector(store => store.dailyEntryReducer);
-  const dispatch = useDispatch();
-  const itemSelected = rows.map((item) => item.prescription_name);
-  console.log('PROPS', props);
-
-  
-  // if (numSelected === itemSelected) {
-  //   setNewSelected(newSelected);
-  //     return;
-  // }
-  // setNewSelected;
-
-  // const clickDelete = (event) => {
-  //   if (numSelected === itemSelected) {
-  //     setNewSelected(newSelected);
-  //       return;
-  //   }
-  // };
-
-  return (
-    <Toolbar
-      sx={{
-        pl: { sm: 2 },
-        pr: { xs: 1, sm: 1 },
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        }),
-      }}
-    >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{ flex: '1 1 100%' }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Entry History
-        </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton onClick={() => dispatch({ type: 'DELETE_ENTRY', payload: numSelected })}>
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Toolbar>
-  );
-}
-
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
 function EntryHistoryTest() {
   const [order, setOrder] = React.useState('asc');
   const [orderBy, setOrderBy] = React.useState('dosage');
-  const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-
-  // ---
-
-  // function createData(prescription_name, prescription_amount, addDate, quantity, notes) {
-  //   return {
-  //     prescription_name,
-  //     prescription_amount,
-  //     addDate,
-  //     quantity,
-  //     notes,
-  //   };
-  // }
-
   console.log('in EntryHistory');
 
     const dispatch = useDispatch();
-    // const history = useHistory();
+    const history = useHistory();
     const rows = useSelector(store => store.dailyEntryReducer);
     console.log('user history ****', rows);
 
@@ -252,41 +161,10 @@ function EntryHistoryTest() {
         })
     }, [])
 
-  // ---
-
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
-  };
-
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.prescription_name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, prescription_name) => {
-    const selectedIndex = selected.indexOf(prescription_name);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, prescription_name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-
-    setSelected(newSelected);
   };
 
   const handleChangePage = (event, newPage) => {
@@ -302,17 +180,6 @@ function EntryHistoryTest() {
     setDense(event.target.checked);
   };
 
-//   const onHandleDelete = (evt) => {
-//     evt.preventDefault();
-//     dispatch({
-//         type: 'DELETE_ENTRY',
-//         payload: editPrescriptions
-//     })
-//     history.push('/home');
-// }
-
-  const isSelected = (prescription_name) => selected.indexOf(prescription_name) !== -1;
-
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
@@ -320,7 +187,6 @@ function EntryHistoryTest() {
   return (
     <Box sx={{ width: '100%' }}>
       <Paper sx={{ width: '100%', mb: 2 }}>
-        <EnhancedTableToolbar numSelected={selected.length} />
         <TableContainer>
           <Table
             sx={{ minWidth: 750 }}
@@ -328,10 +194,8 @@ function EntryHistoryTest() {
             size={dense ? 'small' : 'medium'}
           >
             <EnhancedTableHead
-              numSelected={selected.length}
               order={order}
               orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
               rowCount={rows.length}
             />
@@ -341,41 +205,44 @@ function EntryHistoryTest() {
               {stableSort(rows, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
-                  const isItemSelected = isSelected(row.prescription_name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
 
                   return (
                     <TableRow
                       hover
-                      onClick={(event) => handleClick(event, row.prescription_name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
                       key={row.id} // added id here
-                      selected={isItemSelected}
                     >
                       <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            'aria-labelledby': labelId,
-                          }}
-                        />
                       </TableCell>
                       <TableCell
                         component="th"
-                        id={labelId}
                         scope="row"
                         padding="none"
                       >
                         {row.prescription_name}
                       </TableCell>
-                      {/* <TableCell align="right">{row.prescription_name}</TableCell> */}
                       <TableCell align="right">{row.prescription_amount}</TableCell>
                       <TableCell align="right">{new Date(row.addDate).toLocaleString()}</TableCell>
                       <TableCell align="right">{row.quantity}</TableCell>
                       <TableCell align="right">{row.notes}</TableCell>
+                      <TableCell align="right"> 
+                          <IconButton 
+                            label="Edit Entry"
+                            variant="outlined"
+                            onClick={() => dispatch({ type: 'DELETE_ENTRY', payload: row.id })}
+                          > 
+                            <EditTwoToneIcon /> 
+                          </IconButton>
+                        </TableCell>
+                      <TableCell align="right"> 
+                          <IconButton 
+                            aria-label="delete"
+                            onClick={() => dispatch({ type: 'DELETE_ENTRY', payload: row.id })}
+                          > 
+                            <DeleteIcon /> 
+                          </IconButton>
+                        </TableCell>
+                        <TableCell padding="checkbox">
+                      </TableCell>
                     </TableRow>
                   );
                 })}
